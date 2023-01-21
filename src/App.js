33c00ba1797fb575cf92318f82e2b97cc14a818d
Martin Angeloni <angelonimartin90@gmail.com>
2023-01-21 08:25:15 -0300

    feat: button effects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
+import styled from "styled-components";
 import Square from "./components/Square";
+
+const MainWrapper = styled.div`
+  font-family: "Trebuchet MS", Helvetica, sans-serif;
+  button{
+    border-radius: 20px;
+    width: 5%;
+    height: 30px;
+    margin: 20px;
+    cursor: pointer;
+    transition: all 0.5s ease-out;
+    box-shadow: 5px 5px 5px rgba(68, 68, 68, 0.6);
+    &:hover{
+      box-shadow: 5px 16px 5px rgba(68, 68, 68, 0.6);
+      transform: translateY(-4px);
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
+    <MainWrapper className="App">
       <header>
         Pig Game
       </header>
       <Square />
-    </div>
+    </MainWrapper>
   );
 }
 
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,30 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Controls({ status = 1, onRollDice, onHold, onNewGame }){
-  const [diceStatus, setDiceStatus] = useState(0);
+function Controls({ onHold, onNewGame }){
   return (
     <div>
-      <button onClick={
-        () => {
-          const diceRolled = Math.floor(Math.random() * 6) + 1;
-          onRollDice(diceRolled);
-          setDiceStatus(diceRolled);
-        }}
-      >
-        Roll Dice
-      </button>
       <button onClick={onHold}>
         Hold
       </button>
       <button onClick={onNewGame}>
         New game
       </button>
-      <div>
-        Dice result
-        <div>
-          {diceStatus}
-        </div>
-      </div>
     </div>
   );
 }
